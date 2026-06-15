af8b150131d071d0000e5bd86ca1d0619da21b1f
Andrew Brown <andrew.brown@rossvideo.com>
2026-06-15 15:47:11 -0400

    proper value missing check

--- tools/mandatory.js
+++ tools/mandatory.js
@@ -108,7 +108,7 @@ function validateRequiredParamsAndScopes(deviceDesc) {
         const field = productValue?.struct_value?.fields?.[key];
         const stringValue = field?.string_value;
 
-        if (!stringValue) {
+        if (stringValue === undefined) {
             errors.push({ message: `Product parameter '${key}' has no value`, instancePath: `${basePath}/value` });
         } else if (String(stringValue).trim() === '') {
             errors.push({ message: `Product parameter '${key}' has empty string value`, instancePath: `${basePath}/value/string_value` });
